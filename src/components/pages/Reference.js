5047336759452b4041f38c3a28347b842cce34a3
Cherin0115 <wjddmswl3712@gmail.com>
2021-11-11 18:27:47 +0900

    21.11.11 json 수정

--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -19,7 +19,7 @@ class Reference extends React.Component {
             data: {
                 data: {htmlRefer},
             },
-        } = await axios.get("https://webstoryboy.github.io/react5001/src/assets/json/refer.json");
+        } = await axios.get("https://rlozib.github.io/react99-master/src/assets/json/refer.json");
         this.setState({ refers : htmlRefer, isLoading: false })
 
         console.log(htmlRefer)
